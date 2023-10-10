d24fad617cbc45bfc04d6e3c66e712f8bc389315
Jhordy Gavinchu <65508346+jhordyess@users.noreply.github.com>
2023-10-10 19:12:36 -0400

    Initial commit

---
+++ src/Home.tsx
@@ -0,0 +1,14 @@
+const Home = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <header className="flex flex-col items-center justify-center">
+        <h1 className="text-4xl font-bold">Hello World!</h1>
+      </header>
+      <main className="flex flex-1 flex-col items-center justify-center px-20 text-center">
+        Fugiat veniam cillum tempor ipsum sint quis in commodo sint mollit tempor.
+      </main>
+    </div>
+  )
+}
+
+export default Home
