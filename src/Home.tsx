6a7cf9deef9f615fd1db740f370b5d212224f490
jhordyess <dev@jhordyess.com>
2023-10-10 23:21:29 +0000

    Installed and updated

--- src/Home.tsx
+++ src/Home.tsx
@@ -7,6 +7,25 @@ const Home = () => {
       <main className="flex flex-1 flex-col items-center justify-center px-20 text-center">
         Fugiat veniam cillum tempor ipsum sint quis in commodo sint mollit tempor.
       </main>
+      <footer className="w-full pt-6 text-center">
+        Made with 💪 by&nbsp;
+        <a
+          href="https://www.jhordyess.com"
+          target="_blank"
+          className="text-blue-500 hover:underline"
+        >
+          Jhordyess
+        </a>
+        <br />
+        👉&nbsp;
+        <a
+          href="https://github.com/jhordyess/viewport-calculator"
+          target="_blank"
+          className="text-blue-500 hover:underline"
+        >
+          View on GitHub
+        </a>
+      </footer>
     </div>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/react-tailwind-ts-boilerplate/',
+  base: '/viewport-calculator/',
   resolve: { alias: { '@': '/src' } }
 })
